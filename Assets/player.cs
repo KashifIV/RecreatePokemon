7d13c0d07a62dfbef82c62cd700b93171f4e9374
KashifIV <kashif.h77@gmail.com>
2019-06-27 17:44:57 -0400

    GameObjects and Components

---
+++ Assets/player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    Transform playerPosition; 
+    public float speed = 5; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerPosition = this.GetComponent<Transform>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D)){
+            playerPosition.position = new Vector3(playerPosition.position.x + speed, playerPosition.position.y, playerPosition.position.z); 
+        }
+        else if (Input.GetKey(KeyCode.A)){
+            playerPosition.position = new Vector3(playerPosition.position.x - speed, playerPosition.position.y, playerPosition.position.z); 
+        }
+        else if(Input.GetKey(KeyCode.W)){
+            playerPosition.position = new Vector3(playerPosition.position.x, playerPosition.position.y + speed, playerPosition.position.z); 
+        }
+        else if (Input.GetKey(KeyCode.S)){
+             playerPosition.position = new Vector3(playerPosition.position.x, playerPosition.position.y - speed, playerPosition.position.z); 
+        }
+
+    }
+}
